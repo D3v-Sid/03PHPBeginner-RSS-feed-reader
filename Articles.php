6c9b743d307edd188b07f45382eef935b21179ff
D3v-Sid <Dev-Sid@protonmail.com>
2022-04-26 10:02:02 +0200

    bug : image is now displaying

--- Articles.php
+++ Articles.php
@@ -5,15 +5,15 @@ class Articles{
     public string $fullDescription;
     public string $short_description;
     public $date;
-    public $imageURL;
-    public $link;
+    public string $imageURL;
+    public string $link;
 
     public function __construct(object $item, $imageURL){
         $this->title =  $item->title;
         $this->fullDescription = $item->description ;
         $this->short_description = substr($this->fullDescription,0, strpos( $this->fullDescription, "</p>"));
         $this->date = date('d/m/Y',strtotime( $item->pubDate));
-        $this->imageURL = $imageURL;
+        $this->imageURL = "$imageURL";
         $this->link = $item->link;
     }
     
@@ -24,8 +24,8 @@ class Articles{
                 <h3> $this->title </h3>
                 <h5> $this->date </h5>
             </hgroup>
-            <div class='grid>
-                <img src = '$this->imageURL' height=300 width=200>
+            <div class='grid'>
+                <img src='$this->imageURL' height='300' width='200'>
                 <div>
                     <p>$this->short_description</p>
                     <a href=$this->link> Read More </a> 
